d0cc0b6cbda5a310df97d4946942629a1d758abd
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-09 13:14:22 +0200

    adding working argh through fetchcontent

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <argh.h>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+int main([[maybe_unused]] int argc, char** argv)
 {
     std::cout << "Hello world!\n";
+
+    argh::parser cmdl(argv);
+
+    if (cmdl[{ "-v", "--verbose" }])
+        std::cout << "Verbose, I am.\n";
+
     return 0;
 }
